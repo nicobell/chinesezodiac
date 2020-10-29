cc15beca3a405497624aa06b0e7995a6a9ea31cd
nicobell <nico.bellucco@gmail.com>
2020-10-29 15:01:37 +0100

    commit

---
+++ ChineseZodiac.js
@@ -0,0 +1,80 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+const Alexa = require('ask-sdk');
+let skill;
+
+exports.handler = async function (event, context) {
+    //console.log('REQUEST ' + JSON.stringify(event));
+    if (!skill) {
+        skill = Alexa.SkillBuilders.custom()
+            .addErrorHandlers(ErrorHandler)
+            .addRequestHandlers(
+                LaunchRequestHandler,
+                ChineseZodiacHandler
+            ).create();
+    }
+
+    const response = await skill.invoke(event, context);
+    //console.log('RESPONSE :' + JSON.stringify(response));
+    return response;
+};
+
+const ChineseZodiacHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'ChineseZodiac';
+    },
+    async handle(handlerInput) {
+        
+        const year = Number(Alexa.getSlotValue(handlerInput.requestEnvelope, 'year'));
+        let speechText = 'none';
+        try {
+            let data = await ddb.get({
+                TableName: "ChineseAnimal",
+                Key: {
+                    BirthYear: year
+                }
+            }).promise();
+            speechText = 'your animal is ' + data.Item.Animal + ' which is ' + data.Item.Personality;
+
+        } catch (err) {
+            speechText = 'I do not know the animal for the year ' + year;
+        };
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+};
+
+
+const ErrorHandler = {
+    canHandle(handlerInput) {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log('Error handled: ' + JSON.stringify(error.message));
+        // console.log('Original Request was:', JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
+
+        const speechText = 'Sorry, your skill encountered an error';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+};
+
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Welcome to chinese zodiac animal app';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+};
